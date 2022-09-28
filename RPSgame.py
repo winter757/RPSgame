a64018fc3f9349dccec41ce8aa2572839f374b4f
winter757 <114052140+winter757@users.noreply.github.com>
2022-09-28 08:19:10 +0300

    The game now repeats.

--- RPSgame.py
+++ RPSgame.py
@@ -1,13 +1,28 @@
-human_turn = input("Input human turn: ")
-computer_turn = input("Input computer turn: ")
+import random
 
-if human_turn == "Rock" and computer_turn == "Scissors":
-    print("Human wins!")
-elif human_turn == "Paper" and computer_turn == "Rock":
-    print("Human wins!")
-elif human_turn == "Scissors" and computer_turn == "Paper":
-    print("Human wins!")
-elif human_turn == computer_turn:
-    print("Draw.")
-else:
-    print("Computer wins!")
+def play_again():
+
+    turns = ["Rock", "Paper", "Scissors"]
+    human_turn = input("Input human turn: ")
+    computer_turn = random.choice(turns)
+    print(f"Human: {human_turn} vs. Computer: {computer_turn}")
+
+    if human_turn == "Rock" and computer_turn == "Scissors":
+        print("Human wins!")
+    elif human_turn == "Paper" and computer_turn == "Rock":
+        print("Human wins!")
+    elif human_turn == "Scissors" and computer_turn == "Paper":
+        print("Human wins!")
+    elif human_turn == computer_turn:
+        print("Draw.")
+    else:
+        print("Computer wins!")
+
+while True:
+    restart = input("Do you want to play again? Y/N: ")
+    if restart == "Y":
+        play_again()
+    elif restart == "N":
+        break
+    else:
+        print("Enter a valid answer.")
